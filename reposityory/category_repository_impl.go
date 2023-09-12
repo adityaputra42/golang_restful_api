95f256025aaae92aaa1a968debe0eb623be2aef6
adityaputra42 <aditiyaputra42@gmail.com>
2023-09-12 11:40:00 +0700

    add category repository

---
+++ reposityory/category_repository_impl.go
@@ -0,0 +1,75 @@
+package reposityory
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"golang_restful_api/helper"
+	"golang_restful_api/model/domain"
+)
+
+type CategoryRepositoryImpl struct {
+}
+
+// Delete implements CategoryRepository.
+func (*CategoryRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, category domain.Category) {
+	SQL := "delete from category where id = ?"
+	_, err := tx.ExecContext(ctx, SQL, category.Id)
+	helper.PanicIfError(err)
+
+}
+
+// FindAll implements CategoryRepository.
+func (*CategoryRepositoryImpl) FindAll(ctx context.Context, tx *sql.Tx) []domain.Category {
+	SQL := "select id, name from category"
+	rows, err := tx.QueryContext(ctx, SQL)
+	helper.PanicIfError(err)
+	defer rows.Close()
+
+	var categories []domain.Category
+	for rows.Next() {
+		category := domain.Category{}
+		err := rows.Scan(&category.Id, &category.Name)
+		helper.PanicIfError(err)
+		categories = append(categories, category)
+	}
+	return categories
+}
+
+// FindById implements CategoryRepository.
+func (*CategoryRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, categoryId int) (domain.Category, error) {
+	SQL := "select id, name from category where id = ?"
+	rows, err := tx.QueryContext(ctx, SQL, categoryId)
+	category := domain.Category{}
+	helper.PanicIfError(err)
+	defer rows.Close()
+	if rows.Next() {
+		err := rows.Scan(&category.Id, &category.Name)
+		helper.PanicIfError(err)
+		return category, nil
+
+	} else {
+		return category, errors.New("category is not found")
+	}
+
+}
+
+// Save implements CategoryRepository.
+func (*CategoryRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
+	SQL := "insert into category(name) values(?)"
+	result, err := tx.ExecContext(ctx, SQL, category.Name)
+	helper.PanicIfError(err)
+	id, err := result.LastInsertId()
+	helper.PanicIfError(err)
+	category.Id = int(id)
+	return category
+
+}
+
+// Update implements CategoryRepository.
+func (*CategoryRepositoryImpl) Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
+	SQL := "update category set name = ? where id = ?"
+	_, err := tx.ExecContext(ctx, SQL, category.Name, category.Id)
+	helper.PanicIfError(err)
+	return category
+}
